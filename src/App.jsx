d1f5de343f7cb99e9be64cc890293ddaa338e0d2
xoventechdev <alkarim.doha@gmail.com>
2024-08-20 10:49:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-4 sm:flex sm:items-center sm:py-4 sm:space-y-0 sm:space-x-6">
+        <img
+          className="h-40 mx-auto rounded-full ring-4 ring-gray-300 sm:mx-0 sm:flex-shrink-0"
+          src="/hosen.png"
+          alt="hosen"
+        />
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">Md Kamal Hosen</p>
+            <p className="text-gray-500 font-medium">Web Developer</p>
+          </div>
+          <button className="px-4 py-1 text-sm rounded-full border border-gray-400 text-fuchsia-600">
+            Contact
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
